17b041329a0787894f50233a916190e14eaa6c12
Messi0914 <buddhikabv0914@gmail.com>
2022-05-21 21:29:10 +0800

    Edited update php file

--- appointment/updateApp.php
+++ appointment/updateApp.php
@@ -1,5 +1,23 @@
 <?php
-include('include/connection.php');
+
+  $server_name = "localhost";
+  $user_name = "root";
+  $password = "";
+  $database = "buddhi";
+
+  $connection = new mysqli($server_name , $user_name , $password , $database);
+
+
+  if($connection -> connect_error ){
+
+    die("Connection error");
+
+
+  }else {
+
+    echo("");
+  }
+
 
 ?>
 

e54f46eb1573720b22feccda3087b213b2cfeaa7
thimira789 <thimira.thulanka7@gmail.com>
2022-05-22 20:51:44 +0530

    add styles to check mail page

--- eCare/checkmail.php
+++ eCare/checkmail.php
@@ -4,10 +4,23 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>check your mail</title>
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
 
-	<center><h1>Please check you E mail</h1></center>
+	
+  <div class="jumbotron text-center">
+  <h1 class="display-3">Thank You!</h1>
+  <p class="lead"><strong>Please check your email</strong> for further instructions on how to complete your account setup.</p>
+  <hr>
+  <p>
+    Having trouble? <a href="">Contact us</a>
+  </p>
+  <p class="lead">
+    <a class="btn btn-primary btn-sm" href="https://bootstrapcreative.com/" role="button">Refresh</a>
+  </p>
+</div>
 
 </body>
 </html>
